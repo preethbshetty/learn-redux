abab2cc4ec66c68c88514085a669ac95413395db
preethbshetty <preethbshetty@gmail.com>
2016-07-11 10:10:34 +0530

    Add files via upload

---
+++ client/store.js
@@ -0,0 +1,33 @@
+import {createStore, compose} from 'redux';
+import {syncHistoryWithStore} from 'react-router-redux';
+import {browserHistory} from 'react-router';
+
+//import the root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+//create an object for default data
+const defaultState = {
+  posts,
+  comments
+};
+//const enhancers = compose(
+//  windows.devToolsExtention ? windows.devToolsExtention() | f=> f
+//)
+
+//const store = createStore(rootReducer, defaultState, enhancers);
+const store = createStore(rootReducer, defaultState);
+
+const history = syncHistoryWithStore(browserHistory, store);
+export default history;
+
+if(module.hot) {
+  module.hot.accept('./reducers/',() => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
+export default store;
